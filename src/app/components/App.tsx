e89758b6750ef91d3d5320346b1a8c71e1f67ecd
maxkrieger <themaxaxis@gmail.com>
2020-03-17 17:09:44 -0400

    Cleanup, upgrade packages

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -78,16 +78,6 @@ const App = ({}) => {
         ]);
     }, []);
 
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = event => {
-            const {type, message} = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            }
-        };
-    }, []);
-
     return (
         <div>
             <AceEditor
